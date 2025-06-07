37bb13484ef1e0622fd53c489830f79294d9764a
Diego Ricaurte <daricaurte.ext@gmv.com>
2025-06-06 22:07:31 -0500

    root color sheme

--- web/themes/custom/pwa_testing/js/src/components/paragraph/text-image.js
+++ web/themes/custom/pwa_testing/js/src/components/paragraph/text-image.js
@@ -1,23 +1,10 @@
 // Text Image Paragraph Component
 (function (Drupal) {
-  'use strict';
+  "use strict";
 
   Drupal.behaviors.textImageParagraph = {
     attach: function (context, settings) {
-      const textImageElements = context.querySelectorAll('.paragraph--type--text-image');
-      
-      textImageElements.forEach(element => {
-        console.log('Text Image paragraph initialized');
-        
-        // Ejemplo: lazy loading de imágenes
-        const images = element.querySelectorAll('img');
-        images.forEach(img => {
-          if ('loading' in HTMLImageElement.prototype) {
-            img.loading = 'lazy';
-          }
-        });
-      });
-    }
+      console.log("Drupal behavior textImageParagraph attached");
+    },
   };
-
 })(Drupal);
